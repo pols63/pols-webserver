cf4762fce2d4820fceb946d7d9934b7766136cf1
jean.sanchez@grupotawa.com <jean.sanchez@grupotawa.com>
2025-02-14 09:04:39 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -79,7 +79,7 @@ export class PRoute {
 	readonly session: PSession
 	whiteList: string[] = []
 	blackList: string[] = []
-	finally?(): Promise<void>
+	onFinally?(): Promise<void>
 
 	constructor(server: PWebServer, request: PRequest, session: PSession) {
 		this.server = server
